5436f1c8e4533e1903195e3e108372a827702125
1666sApple <ananno.034@gmail.com>
2025-05-26 03:13:59 +0600

    Initialize Project Structure

---
+++ src/frontend_setup/core/__init__.py
@@ -0,0 +1,8 @@
+"""
+Core functionality for React Setup CLI
+"""
+
+from frontend_setup.core.base import BaseSetup
+from frontend_setup.core.validator import SystemValidator
+
+__all__ = ["BaseSetup", "SystemValidator"]
